71f6cd456d25aaeec82eff93c20a1b01681dfda6
kazi331 <kazisharifulislam52@gmail.com>
2023-01-31 16:20:38 +0600

    filter with stock and brand added

--- src/pages/Main/Cart.js
+++ src/pages/Main/Cart.js
@@ -3,8 +3,7 @@ import { useSelector } from "react-redux";
 import ProductCard from "../../components/ProductCard";
 
 const Cart = () => {
-  const cart = useSelector((state) => state.cart);
-  console.log(cart)
+  const cart = useSelector((state) => state.product.cart);
   return (<>
     {!cart.length && <p className="py-10 text-bold text-lg text-center text-pink-500">No items found on Cart</p>}
     <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 max-w-7xl gap-14 mx-auto my-10'>
--- src/pages/Main/Home.js
+++ src/pages/Main/Home.js
@@ -13,19 +13,34 @@ const Home = () => {
 
   const activeClass = "text-white  bg-indigo-500 border-white";
 
-  const {filter} = useSelector(state => state.filter)
-  console.log(filter.brand)
+  const { filter } = useSelector(state => state.filter)
+  const { brand, stock } = filter;
+
+  let content = products.length && products.map((product) => <ProductCard key={product.model} product={product} />);
+  // filter content with brand and stock
+
+  if (products.length && (stock || brand.length)) {
+    content = products
+      .filter(product => stock ? product.status === true : product) // ternary
+      .filter(product => {
+        if (brand.length) {
+          return brand.includes(product.brand)
+        }
+        return product;
+      })
+      .map((product) => <ProductCard key={product.model} product={product} />);
+  }
 
 
   return (
     <div className='max-w-7xl gap-14 mx-auto my-10'>
       <div className='mb-10 flex justify-end gap-5'>
-        <button className={`border px-3 py-2 rounded-full font-semibold ${filter.stock && activeClass} `} onClick={() => dispatch({type: TOGGLE_STOCK}) }>In Stock</button>
-        <button className={`border px-3 py-2 rounded-full font-semibold ${filter.brand.includes('amd') && activeClass}`} onClick={() => dispatch({type: TOGGLE_BRAND, payload: 'amd'})}> AMD</button>
-        <button className={`border px-3 py-2 rounded-full font-semibold ${filter.brand.includes('intel') && activeClass} `} onClick={() => dispatch({type: TOGGLE_BRAND, payload: 'intel'})}> Intel</button>
+        <button className={`border px-3 py-2 rounded-full font-semibold ${stock && activeClass} `} onClick={() => dispatch({ type: TOGGLE_STOCK })}>In Stock</button>
+        <button className={`border px-3 py-2 rounded-full font-semibold ${brand.includes('amd') && activeClass}`} onClick={() => dispatch({ type: TOGGLE_BRAND, payload: 'amd' })}> AMD</button>
+        <button className={`border px-3 py-2 rounded-full font-semibold ${brand.includes('intel') && activeClass} `} onClick={() => dispatch({ type: TOGGLE_BRAND, payload: 'intel' })}> Intel</button>
       </div>
       <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-14'>
-        {products.map((product) => <ProductCard key={product.model} product={product} />)}
+        {content}
       </div>
     </div>
   );
